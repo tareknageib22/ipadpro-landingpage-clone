d48514baec194364e998cbd5cb7b038b46c8548e
Tarek nageib <tarek.nageib303@gmail.com>
2025-07-06 06:52:07 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
+    gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
 
-  ScrollSmoother.create({
-    wrapper: "#smooth-wrapper",
-    content: "#smooth-content",
-    smooth: 1,
-    effects: true,
-    smoothTouch: 0.1,
-  });
+    ScrollSmoother.create({
+        wrapper: "#smooth-wrapper",
+        content: "#smooth-content",
+        smooth: 1,
+        effects: true,
+        smoothTouch: 0.1,
+    });
 
-  const navComponentWrapper = document.getElementById("all-dropmenu-wrapper");
-  const navLinks = gsap.utils.toArray("nav a[data-target]");
+    const all_Dropmenu_Wrapper = document.getElementById("all-dropmenu-wrapper");
+    const all_NavLinks = gsap.utils.toArray("nav a[data-target]");
 
-  navLinks.forEach(link => {
-    const targetId = link.getAttribute("data-target");
-    const menuWrapper = document.getElementById(targetId);
-    if (!menuWrapper || !navComponentWrapper) return;
+    all_NavLinks.forEach(link => {
+        const targetId = link.getAttribute("data-target");
+        const drop_menuWrapper = document.getElementById(targetId);
+        if (!drop_menuWrapper || !all_Dropmenu_Wrapper) return;
 
-    // Get the inner links
-    const innerLinks = menuWrapper.querySelectorAll("a");
+        // Get the inner links
+        const innerLinks = drop_menuWrapper.querySelectorAll("a");
 
-    console.log("menu wrapper",menuWrapper)
-    console.log("nav compoenet wrapper",navComponentWrapper)
-    console.log("navLinks",navLinks)
-    console.log("targetiD",targetId)
-// paste it here 
-  });
+        console.log("all_NavLinks", all_NavLinks)
+        console.log("links-target-id", link)
+        console.log("all_Dropmenu_Wrapper", all_Dropmenu_Wrapper)
+        console.log("drop_menuWrapper", drop_menuWrapper)
+        console.log("targetiD", targetId)
+        // paste it here 
+
+
+        
+    });
 });
